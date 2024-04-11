320b41b3837a2f2d38a60085ffde9c4ee70af369
BuildNg <nguyenhoangtrung2004@gmail.com>
2024-04-10 22:39:58 -0400

    update starterpack

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import './style.scss';
-
-const mainElement = document.getElementById('main');
-let seconds = 0; // Counter for the number of seconds
-
-setInterval(() => {
-  seconds += 1; // Increment the counter each second
-  mainElement.textContent = `You've been on this page for ${seconds} seconds.`; // Update the text content of the main element
-}, 1000);
